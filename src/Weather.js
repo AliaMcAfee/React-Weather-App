fbdf8b7d22632a23fd85ec3e161be483d9b382f9
AliaMcAfee <mcafeealia@gmail.com>
2025-01-20 21:18:35 -0600

    Added content

---
+++ src/Weather.js
@@ -0,0 +1,52 @@
+import axios from "axios";
+import react, { useState } from "react";
+
+export default function City() {
+  const [city, setCity] = useState("");
+  const [weather, setWeather] = useState({});
+  const [upload, setUpload] = useState(false);
+
+  function displayWeather(response) {
+    setUpload(true);
+    setWeather({
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      description: response.data.weather[0].description,
+    });
+  }
+
+  function Submit(event) {
+    event.preventDefault();
+    let apiKey = "094780c710fa4efd669f0df8c3991927";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayWeather);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  let form = (
+    <form onSubmit={Submit}>
+      <input type="search" placeholder="Enter a city.." onChange={updateCity} />
+      <button type="Submit">Search</button>
+    </form>
+  );
+
+  if (upload) {
+    return (
+      <div>
+        {form}
+        <ul>
+          <li>Temperature: {Math.round(weather.temperature)}°C</li>
+          <li>Description: {weather.description}</li>
+          <li>Humidity: {weather.humidity}%</li>
+          <li>Wind: {weather.wind}km/h</li>
+        </ul>
+      </div>
+    );
+  } else {
+    return form;
+  }
+}
