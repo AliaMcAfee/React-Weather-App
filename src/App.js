155bf22df56ba8f4dcb2b6853b8f8e2e7c3552c2
AliaMcAfee <mcafeealia@gmail.com>
2025-01-20 21:50:26 -0600

    added sources

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ export default function App() {
     <div className="App">
       <h1>Weather App</h1>
       <Weather />
+      <p>
+        This project was coded by Alia McAfee and is open-sourced on{" "}
+        <a href="https://github.com/AliaMcAfee/React-Weather-App.git">GitHub</a>
+      </p>
     </div>
   );
 }
