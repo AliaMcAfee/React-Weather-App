9f9afc4a551d7f43eaa9c8fe8eee48652e40d967
AliaMcAfee <mcafeealia@gmail.com>
2025-01-21 17:29:31 -0600

    removed footer and logo.svg

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Weather from "./Weather";
 
@@ -7,11 +6,6 @@ export default function App() {
     <div className="App">
       <h1>Weather App</h1>
       <Weather />
-      <p>
-        This project was coded by Alia McAfee and is open-sourced on{" "}
-        <a href="https://github.com/AliaMcAfee/React-Weather-App.git">GitHub</a>{" "}
-        and hosted on <a href="">Netlify</a>
-      </p>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import react, { useState } from "react";
+import React, { useState } from "react";
 
 export default function City() {
   const [city, setCity] = useState("");
